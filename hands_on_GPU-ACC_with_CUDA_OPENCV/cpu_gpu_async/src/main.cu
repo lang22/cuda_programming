e89af9d03327e9084c937b5961e3773ddb6d56c2
lovelydett <xyt@bupt.cn>
2022-02-18 14:35:31 +0800

    [Hands] cpu_gpu_sync and chrono profile

---
+++ hands_on_GPU-ACC_with_CUDA_OPENCV/cpu_gpu_async/src/main.cu
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <cuda.h>
+#include <chrono>
+
+__global__ void kernel_print(void) {
+	printf("Hello from %d, %d\n", blockIdx.x, threadIdx.x);
+}
+
+std::chrono::time_point<std::chrono::system_clock> t1;
+std::chrono::time_point<std::chrono::system_clock> t2;
+
+inline void time_checkpoint(bool start = false) {
+	if (start) {
+		t1 = std::chrono::system_clock::now();
+		return;
+	}
+	t2 = std::chrono::system_clock::now();
+	auto duration = t2 - t1;
+	printf("%ld us\n", std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count());
+	t1 = std::chrono::system_clock::now();
+}
+
+
+int main(void) {
+	time_checkpoint(true);
+	kernel_print<<<16, 4>>>();
+	time_checkpoint();
+	for(int i = 0; i < 10000; ++i) {
+		if (i % 100 == 0) {
+			printf("\t\tHello in main thread\n");
+		}
+	}
+	time_checkpoint();
+	cudaDeviceSynchronize(); // w/o this line, nothing from kernel_print would be printed!
+	time_checkpoint();
+	printf("All finished\n");
+	return 0;
+}
